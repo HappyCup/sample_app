ecc84df306dda6cc6c65f4214066fd2f8f8697ef
HappyCup <1274474220qq.com>
2013-04-01 10:48:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4d869865672f7428f00b52fb943e76012c9ec32740efe35ff91df7e4d8af891a53d0e9d053e48c148dd230274a4d7a78e34a9973be880d8552b8e73e60a669dd'
